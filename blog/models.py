fa9ecda3abc8603d77b4d2476841d7c02b0ced81
AwaleRohin <rohinawale331@gmail.com>
2019-01-18 08:36:20 +0545

    Post urls and views created

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+
+
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete = models.CASCADE)
-    tittle = models.CharField(max_length = 200) #CharField chai limited huncha
-    text = models.TextField() #TextField chai unlimited huncha
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # CharField has limited characters
+    tittle = models.CharField(max_length=200)
+    # TextField has unlimited characters
+    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date=timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
+    def __str__(self):
+        return self.tittle
