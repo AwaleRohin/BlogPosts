6b120f086fca42883056803460af29a78e153d2f
AwaleRohin <rohinawale331@gmail.com>
2019-01-23 19:15:58 +0545

    Html file updated and Comment Model Created

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.tittle
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
